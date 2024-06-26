eaadf4f9f64273da54bd882980a8b59bc7971751
Goen Hee Han <gksrjsgml13@naver.com>
2024-06-26 16:06:33 +0900

    DbContext 및 UI 상관관계 테스트

---
+++ Models/MyDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPAPP.Models;
+
+public class MyDbContext : DbContext
+{
+    public DbSet<Person> People { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseInMemoryDatabase("MyDb");
+    }
+}
