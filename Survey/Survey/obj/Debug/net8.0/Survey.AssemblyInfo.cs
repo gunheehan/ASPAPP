82f01662c1e2621597dbbcca224ddd9388ec18e3
Goen Hee Han <gksrjsgml13@naver.com>
2024-07-27 22:09:09 +0900

    newform 생성 페이지 추가 및 홈연결

--- Survey/Survey/obj/Debug/net8.0/Survey.AssemblyInfo.cs
+++ Survey/Survey/obj/Debug/net8.0/Survey.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Survey")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+058c56d949e7e37b0d40172d0a048013fad31d7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+655cac1826923c7bcf0476067070b7a1030e230d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Survey")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Survey")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
